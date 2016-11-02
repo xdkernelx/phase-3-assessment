3240f611d73ea33fea0bc4a2166299173abe50f4
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-02 10:43:02 -0700

    Refactor the AJAX call to iterate through the response and return the shortest available description.

--- phase3/app/assets/javascripts/remote.js
+++ phase3/app/assets/javascripts/remote.js
@@ -1,7 +1,13 @@
 $(document).on('turbolinks:load', function() {
 
   $('.pet-toys').on('ajax:success', function(event, response){
-    console.log(response);
+    var shortest = 0;
+    $.each(response, function( index, value ) {
+      if(response[index]['description'].length < response[shortest]['description'].length) {
+        shortest = index;
+      }
+    });
+    console.log(response[shortest]['description']);
   });
 
 });
