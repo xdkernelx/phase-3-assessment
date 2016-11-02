2fac8ee6c4fd95c574e99aecbfe97c26c1ddce2a
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-02 09:22:10 -0700

    Write migration text for the Pet class.

--- phase3/db/migrate/20161102161933_create_pets.rb
+++ phase3/db/migrate/20161102161933_create_pets.rb
@@ -1,8 +1,12 @@
 class CreatePets < ActiveRecord::Migration[5.0]
   def change
     create_table :pets do |t|
+      t.string :name, { limit: 64 }
+      t.string :breed, { limit: 64 }
+      t.integer :age
+      t.boolean :cute
 
-      t.timestamps
+      t.timestamps(null: false)
     end
   end
 end
