a632b5c408168efa466264c68bdc44404fa51d39
Nestor Alvarez <grimreaper1169@gmail.com>
2016-11-02 09:40:49 -0700

    Add a root route.

--- phase3/config/routes.rb
+++ phase3/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'pets#index'
+
   resources :pets, only: [:index, :show] do
     resources :toys, only: [:index, :show]
   end
